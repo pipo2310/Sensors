8c67e067c97f3ae0deb11658113d80aafd949c08
Alessandro Feoli <afeoli07@gmail.com>
2019-09-24 12:05:49 -0600

    Agrega proyecto actualizado con correcto nombramiento

---
+++ Sensores/app/src/main/java/ucr/ac/cr/ecci/ci1340/sensores/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package ucr.ac.cr.ecci.ci1340.sensores.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
