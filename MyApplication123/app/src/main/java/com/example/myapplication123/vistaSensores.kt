f9a7d9ec3bed1e1d2aae73b1564539ae19a237c7
Christian Roberto Rodriguez Bujan <b65970@ecci.ucr.ac.cr>
2019-09-23 18:43:27 -0600

    Agregar y medio visualizar y modificar

---
+++ MyApplication123/app/src/main/java/com/example/myapplication123/vistaSensores.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication123
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class vistaSensores : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vista_sensores)
+    }
+}
