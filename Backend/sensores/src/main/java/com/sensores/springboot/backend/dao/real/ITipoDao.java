8f3c55ae9a5de1a7f461467ad80824ae8544197c
Alessandro Feoli <afeoli07@gmail.com>
2019-10-29 23:29:40 -0600

    Put para costos

--- Backend/sensores/src/main/java/com/sensores/springboot/backend/dao/real/ITipoDao.java
+++ Backend/sensores/src/main/java/com/sensores/springboot/backend/dao/real/ITipoDao.java
@@ -3,5 +3,9 @@ package com.sensores.springboot.backend.dao.real;
 import com.sensores.springboot.backend.model.entity.real.TipoSensor;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ITipoDao extends CrudRepository<TipoSensor, Integer> {
+    List<TipoSensor> findAll();
+    List<TipoSensor> findById(int id);
 }
